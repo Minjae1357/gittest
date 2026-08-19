d43a8b1f133408f69ea6bdd55f196f157434abd3
LSS <dlsang65@naver.com>
2026-08-19 15:49:32 +0900

    haha

--- src/main/java/org/example/gittest/HelloController.java
+++ src/main/java/org/example/gittest/HelloController.java
@@ -1,6 +1,5 @@
 package org.example.gittest;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
